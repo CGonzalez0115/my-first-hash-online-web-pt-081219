bfa13aa1d6048fac1749f8249b6cf4184bc81cbe
Christina Gonzalez <cmgamway@gmail.com>
2019-08-28 04:56:35 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -12,19 +12,7 @@ def retrieval
 end
 
 def adding
-  shipping_manifest = {
-                  "whale bone corsets" => 5,
-                  "porcelain vases" => 2,
-                  "oil paintings" => 3
-                 }
-  # add 2 muskets to the shipping_manifest hash below
-
-
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
-
+  the_manifest = {"whale bone corsets" => 2, "oil paintings" => 5, "porcelain vases" => 3}
+  the_manifest["muskets"] = 2
+  the_manifest["gun powder"] = 4
 end
